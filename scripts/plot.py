d57a7d6489375bd0c4c225694e1ef32c81e53860
ujvl <misraujval@gmail.com>
2020-05-02 21:59:36 -0700

    add CDF plotter

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from __future__ import print_function
 import argparse
 import logging
@@ -12,27 +13,16 @@ logger.setLevel("INFO")
 
 
 def main(args):
+    logger.debug("Args: %s", args)
     data = pd.read_csv(args.fname, sep=args.delim)
-    plotter_cls(args.plt)().plot(data, args)
+    PLOTTERS[args.plt]().plot(data, args)
     decorate_plot(args)
     logger.info("Showing plot...")
     plt.show()
 
 
-def plotter_cls(name):
-    plotters = {
-        "scatter": ScatterPlot,
-        "line": ScatterPlot,
-        "reg": RegressionPlot,
-        "box": BoxPlot,
-    }
-    return plotters[name]
-
-
 def decorate_plot(args):
-    """
-    Requests user input to add plot labels, title etc.
-    """
+    """Requests user input to add plot labels, title etc."""
     title = args.title or input("Title: ")
     plt.title(title)
 
@@ -43,33 +33,42 @@ def decorate_plot(args):
     plt.ylabel(y_label)
 
     if args.x_max:
-        plt.xlim(None, args.x_max)
+        plt.xlim(0, args.x_max)
     if args.y_max:
-        plt.ylim(None, args.y_max)
+        plt.ylim(0, args.y_max)
 
     ax = plt.gca()
-    if args.x_log:
-        ax.set_xscale("symlog")
-    if args.y_log:
-        ax.set_yscale("symlog")
+    if args.x_scale:
+        ax.set_xscale(args.x_scale)
+    if args.y_scale:
+        ax.set_yscale(args.y_scale)
+
+
+def p(n):
+    def p_(x):
+        return np.percentile(x, n)
+    p_.__name__ = "p_%s" % n
+    return p_
 
 
 class ScatterPlot:
     """Scatter plot"""
 
     def plot(self, data, args):
-        if all(col is None for col in [args.x_col, args.y_col, args.x_col]):
+        if all(col is None for col in [args.x_col, args.y_col, args.z_col]):
             cols = data.columns
-            args.x_col, args.y_col, args.z_col = cols[0], cols[1], cols[2]
-            logger.info("Using x=%s, y=%s, z=%s by default.", args.x_col, args.y_col, args.z_col)
+            args.x_col, args.y_col = cols[0], cols[1]
+            args.z_col = cols[2] if len(cols) > 2 else None
+            logger.info("Using x=%s, y=%s, z=%s by default.",
+                        args.x_col, args.y_col, args.z_col)
 
         # Workaround since hue uses duck-typing for numerics.
-        if args.z_col:
+        if args.z_col and data.dtypes[args.z_col] == int:
             data[args.z_col] = data[args.z_col].astype(str)
             data[args.z_col] = "$" + data[args.z_col] + "$"
 
         sns.set()
-        plot = sns.relplot(
+        sns.relplot(
             x=args.x_col,
             y=args.y_col,
             hue=args.z_col,
@@ -82,13 +81,15 @@ class RegressionPlot:
     """Scatter plot with fitted function"""
 
     def plot(self, data, args):
-        if all(col is None for col in [args.x_col, args.y_col, args.x_col]):
+        if all(col is None for col in [args.x_col, args.y_col]):
             cols = data.columns
-            args.x_col, args.y_col, args.z_col = cols[0], cols[1], cols[2]
-            logger.info("Using x=%s, y=%s, z=%s by default.", args.x_col, args.y_col, args.z_col)
+            args.x_col, args.y_col = cols[0], cols[1]
+            args.z_col = cols[2] if len(cols) > 2 else None
+            logger.info("Using x=%s, y=%s, z=%s by default.",
+                        args.x_col, args.y_col, args.z_col)
 
         sns.set()
-        plot = sns.lmplot(x=args.x_col, y=args.y_col, hue=args.z_col, data=data)
+        sns.lmplot(x=args.x_col, y=args.y_col, hue=args.z_col, data=data)
 
 
 class BoxPlot:
@@ -100,7 +101,6 @@ class BoxPlot:
             args.x_col, args.y_col = cols[0], cols[1]
             logger.info("Using x=%s, y=%s by default.", args.x_col, args.y_col)
 
-        p = self.p
         data = data[[args.x_col, args.y_col]]
         aggs = data.groupby(args.x_col).agg(
             ["mean", "std", p(25), p(50), p(75), p(90), p(95), p(99)]
@@ -108,37 +108,71 @@ class BoxPlot:
         logger.info(aggs)
 
         sns.set()
-        boxplt = sns.boxplot(x=args.x_col, y=args.y_col, data=data, palette="Blues", width=0.35);
+        sns.boxplot(
+            x=args.x_col, y=args.y_col, data=data, palette="Blues", width=0.35)
 
-    def p(self, n):
-        def p_(x):
-            return np.percentile(x, n)
-        p_.__name__ = 'p_%s' % n
-        return p_
+
+class CDFPlot:
+
+    def plot(self, data, args):
+        if all(col is None for col in [args.x_col, args.z_col]):
+            cols = data.columns
+            args.x_col, args.z_col = cols[0], cols[1]
+            logger.info("Using x=%s, z=%s by default.", args.x_col, args.z_col)
+
+        # Workaround since hue uses duck-typing for numerics.
+        if args.z_col and data.dtypes[args.z_col] == int:
+            data[args.z_col] = data[args.z_col].astype(str)
+            data[args.z_col] = "$" + data[args.z_col] + "$"
+
+        kwargs = {"cumulative": True}
+        aggs = data.groupby(args.z_col)
+
+        sns.set()
+        for agg in aggs:
+            accs = agg[1]["max_acc"]
+            sns.distplot(
+                accs,
+                hist=False,
+                hist_kws=kwargs,
+                kde_kws=kwargs,
+                bins=50,
+                label=agg[0])
+
+
+PLOTTERS = {
+    "scatter": ScatterPlot,
+    "line": ScatterPlot,
+    "reg": RegressionPlot,
+    "box": BoxPlot,
+    "cdf": CDFPlot,
+}
 
 
 def parse():
-    parser = argparse.ArgumentParser(description='plot some dataz')
+    parser = argparse.ArgumentParser(description="plot some dataz")
     # Data processing
-    parser.add_argument('--fname', type=str, required=True)
-    parser.add_argument('--plt', type=str, choices=["scatter", "line", "reg", "box"], default="scatter")
-    parser.add_argument('--x-col', type=str)
-    parser.add_argument('--y-col', type=str)
-    parser.add_argument('--z-col', type=str)
-    parser.add_argument('--delim', type=str, default=',')
-    # Decorate plot.
-    parser.add_argument('--x-max', type=int)
-    parser.add_argument('--y-max', type=int)
-    parser.add_argument('--x-log', action="store_true")
-    parser.add_argument('--y-log', action="store_true")
-    parser.add_argument('--xlabel', type=str)
-    parser.add_argument('--ylabel', type=str)
-    parser.add_argument('--title', type=str)
+    parser.add_argument("--fname", type=str, required=True)
+    parser.add_argument(
+        "--plt",
+        type=str,
+        choices=list(PLOTTERS.keys()),
+        default="scatter")
+    parser.add_argument("--x-col")
+    parser.add_argument("--y-col")
+    parser.add_argument("--z-col")
+    parser.add_argument("--delim", default=",")
+    # Decorate plot. Omitting some of these may trigger input request.
+    parser.add_argument("--x-max", type=float)
+    parser.add_argument("--y-max", type=float)
+    parser.add_argument("--x-scale")
+    parser.add_argument("--y-scale")
+    parser.add_argument("--xlabel")
+    parser.add_argument("--ylabel")
+    parser.add_argument("--title")
     args = parser.parse_args()
     return args
 
 
-if __name__ == '__main__':
-    args = parse()
-    logger.info("Args: %s", args)
-    main(args)
+if __name__ == "__main__":
+    main(parse())
